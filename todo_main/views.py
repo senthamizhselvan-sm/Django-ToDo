3e1238a0081727e704502ff9954934578e21e319
senthamizhselvan-sm <senthamizhselvan.sm@gmail.com>
2025-11-18 18:49:29 +0530

    completed task

--- todo_main/views.py
+++ todo_main/views.py
@@ -4,7 +4,9 @@ from ToDo_app.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed = False).order_by('-updated_at')
+    is_completed_tasks = Task.objects.filter(is_completed = True)
     context = {
         'tasks' : tasks,
+        'is_completed_tasks' : is_completed_tasks,
     }
     return render(request, "home.html" , context)
