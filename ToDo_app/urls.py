de276a38ff5af8a80c2caa2de722a9c3f96f896d
senthamizhselvan-sm <senthamizhselvan.sm@gmail.com>
2025-11-18 19:15:06 +0530

    mark task as undone

--- ToDo_app/urls.py
+++ ToDo_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('add-task' , views.add_task , name = 'add_task'),
+    path('completed_task/<int:pk>', views.completed_task , name = 'completed_task'),
+    path('incomplete_task/<int:pk>' , views.incomplete_task , name = 'incomplete_task'),
 ]
--- ToDo_app/views.py
+++ ToDo_app/views.py
@@ -1,9 +1,22 @@
 from django.http import HttpResponse
-from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404, redirect
 from .models import Task
 
 # Create your views here.
 def add_task(request):
     task = request.POST['task']
     Task.objects.create(task = task)
+    return redirect('home')
+
+
+def completed_task(request , pk):
+    task = get_object_or_404(Task , pk = pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def incomplete_task(request , pk):
+    task = get_object_or_404(Task , pk=pk)
+    task.is_completed = False
+    task.save()
     return redirect('home')
